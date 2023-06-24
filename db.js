a39636952d099afe0a7cc5d9d8cfa0a99de616b5
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-06-24 15:54:42 +0530

    next ques added

--- db.js
+++ db.js
@@ -33,12 +33,12 @@ const data = new project({
             question: [
                 {
                     heading: "Creating a simple Calculator card.",
-                    info: 'In this task, your goal is to create a user interface for a simple calculator by utilizing HTML. The calculator will be presented as a card, structured with a main <div> element having the class name "calculator-card". Within this card, you will need to include two additional <div> elements to organize the content.'+
-                    '/n' + '/n' +
-                    'The first inner <div>, with the class name "input-div" and insert "0" as default content in this div, this will serve as a container for the calculators input field. This field will be used to display the numbers and results of calculations to the user. ' +
-                    '/n' + '/n' +
-                    'The second inner <div>, with the class name "buttons", will act as a container for the calculators buttons. These buttons will enable users to perform basic mathematical operations such as addition, subtraction, multiplication, and division.',
-                    example: '<div class="class-name">'+'/n'+'/t'+'<div class="nested-class">'+'/n'+'/t'+'</div'+'/n'+'</div>',
+                    info: 'In this task, your goal is to create a user interface for a simple calculator by utilizing HTML. The calculator will be presented as a card, structured with a main <div> element having the class name "calculator-card". Within this card, you will need to include two additional <div> elements to organize the content.' +
+                        '/n' + '/n' +
+                        'The first inner <div>, with the class name "input-div" and insert "0" as default content in this div, this will serve as a container for the calculators input field. This field will be used to display the numbers and results of calculations to the user. ' +
+                        '/n' + '/n' +
+                        'The second inner <div>, with the class name "buttons", will act as a container for the calculators buttons. These buttons will enable users to perform basic mathematical operations such as addition, subtraction, multiplication, and division.',
+                    example: '<div class="class-name">' + '/n' + '/t' + '<div class="nested-class">' + '/n' + '/t' + '</div' + '/n' + '</div>',
                     solution: '<div class="calculator-card">' + '/n' + '/t' + '<div class="input-div">' + '/n' + '/t' + '</div>' + '/n' + '/t' + '<div class="buttons">' + '/n' + '/t' + '</div>' + '/n' + '</div>',
                     difficulty: 'Easy',
                     preview: false
@@ -46,33 +46,53 @@ const data = new project({
                 {
                     heading: 'Adding buttons in Calculator.',
                     info:
-                    'As part of the calculator card creation, you are required to initialize and include 20 button elements inside the <div> element with the class name "buttons". These buttons will provide functionality for various operations and numerical inputs in the calculator.' +'\n' +
-                    'Each button should have a specific content associated with it. The content of the buttons should be initialized in the following order:' +'\n' +
-                    '1. "AC" - Represents the clear all (reset) functionality.' + '\n' +
-                    '2. "DEL" - Represents the delete (backspace) functionality.' + '\n' +
-                    '3. "%" - Represents the percentage functionality.' + '\n' +
-                    '4. "/" - Represents the division operation.' + '\n' +
-                    '5. "7" - Represents the number 7.' + '\n' +
-                    '6. "8" - Represents the number 8.' + '\n' +
-                    '7. "9" - Represents the number 9.' + '\n' +
-                    '8. "*" - Represents the multiplication operation.' +'\n' +
-                    '9. "4" - Represents the number 4.' + '\n' +
-                    '10. "5" - Represents the number 5.' + '\n' +
-                    '11. "6" - Represents the number 6.' + '\n' +
-                    '12. "-" - Represents the subtraction operation.' + '\n' +
-                    '13. "1" - Represents the number 1.' + '\n' +
-                    '14. "2" - Represents the number 2.' + '\n' +
-                    '15. "3" - Represents the number 3.'+ '\n' +
-                    '16. "+" - Represents the addition operation.' + '\n' +
-                    '17. "^" - Represents the exponentiation (power) operation.' + '\n' +
-                    '18. "0" - Represents the number 0.'+ '\n' +
-                    '19. "." - Represents the decimal point.'+ '\n' +
-                    '20. "=" - Represents the calculation (equal) operation.' + '\n' +
-                    'By including these 20 button elements within the "buttons" <div>, you will provide users with a comprehensive set of options to perform various mathematical operations and numerical inputs on the calculator interface.',
+                        'As part of the calculator card creation, you are required to initialize and include 20 button elements inside the <div> element with the class name "buttons". These buttons will provide functionality for various operations and numerical inputs in the calculator.' + '\n' +
+                        'Each button should have a specific content associated with it. The content of the buttons should be initialized in the following order:' + '\n' +
+                        '1. "AC" - Represents the clear all (reset) functionality.' + '\n' +
+                        '2. "DEL" - Represents the delete (backspace) functionality.' + '\n' +
+                        '3. "%" - Represents the percentage functionality.' + '\n' +
+                        '4. "/" - Represents the division operation.' + '\n' +
+                        '5. "7" - Represents the number 7.' + '\n' +
+                        '6. "8" - Represents the number 8.' + '\n' +
+                        '7. "9" - Represents the number 9.' + '\n' +
+                        '8. "*" - Represents the multiplication operation.' + '\n' +
+                        '9. "4" - Represents the number 4.' + '\n' +
+                        '10. "5" - Represents the number 5.' + '\n' +
+                        '11. "6" - Represents the number 6.' + '\n' +
+                        '12. "-" - Represents the subtraction operation.' + '\n' +
+                        '13. "1" - Represents the number 1.' + '\n' +
+                        '14. "2" - Represents the number 2.' + '\n' +
+                        '15. "3" - Represents the number 3.' + '\n' +
+                        '16. "+" - Represents the addition operation.' + '\n' +
+                        '17. "^" - Represents the exponentiation (power) operation.' + '\n' +
+                        '18. "0" - Represents the number 0.' + '\n' +
+                        '19. "." - Represents the decimal point.' + '\n' +
+                        '20. "=" - Represents the calculation (equal) operation.' + '\n' +
+                        'By including these 20 button elements within the "buttons" <div>, you will provide users with a comprehensive set of options to perform various mathematical operations and numerical inputs on the calculator interface.',
                     example: '<button> AC </button>',
                     solution: '<div class="buttons">' + '\n' + '\t' + '<button>AC</button>' + '\n' + '\t' + '<button>DEL</button>' + '\n' + '\t' + '<button>%</button>' + '\n' + '\t' + '<button>/</button>' + '\n' + '\t' + '<button>7</button>' + '\n' + '\t' + '<button>8</button>' + '\n' + '\t' + '<button>9</button>' + '\n' + '\t' + '<button>*</button>' + '\n' + '\t' + '<button>4</button>' + '\n' + '\t' + '<button>5</button>' + '\n' + '\t' + '<button>6</button>' + '\n' + '\t' + '<button>-</button>' + '\n' + '\t' + '<button>1</button>' + '\n' + '\t' + '<button>2</button>' + '\n' + '\t' + '<button>3</button>' + '\n' + '\t' + '<button>+</button>' + '\n' + '\t' + '<button>^</button>' + '\n' + '\t' + '<button>0</button>' + '\n' + '\t' + '<button>.</button>' + '\n' + '\t' + '<button>=</button>' + '\n' + '</div>',
                     difficulty: 'Easy',
                     preview: false
+                },
+                {
+                    heading: 'Center the Calculator card.',
+                    info: 'To create an attractive and user-friendly interface, it is essential to style the <body> tag appropriately. This involves centering the calculator card on the page and selecting a suitable background color.' + '/n' +
+                        'The styling of the body tag should ensure that the calculator card is positioned at the center of the page, while also considering the choice of an appropriate background color.',
+                    example: 'body {' + '/n' + '/t' + 'background-color: #000' + '/n' + '}',
+                    solution: 'body {\n' +
+                        '\tbackground-color: #000;\n' +
+                        '\tdisplay: flex;\n' +
+                        '\tjustify-content: center;\n' +
+                        '\talign-items: center;\n' +
+                        '\tmin-height: 100vh;\n' +
+                        '}',
+                    difficulty: 'Easy',
+                    html: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Calculator</title><link rel="stylesheet" href="ex.css"></head><body><div class="calculator-card"><div class="input-div">0</div><div class="buttons"><button>AC</button><button>DEL</button><button>%</button><button>/</button><button>7</button><button>8</button><button>9</button><button>*</button><button>4</button><button>5</button><button>6</button><button>-</button><button>1</button><button>2</button><button>3</button><button>+</button><button>^</button><button>0</button><button>.</button><button>=</button></div></div><script src="ex.js"></script></body></html>',
+                    css: '* { margin: 0; padding: 0; box-sizing: border-box; } body { background-color: #000; display: flex; justify-content: center; align-items: center; min-height: 100vh; }',
+                    preview: true
+                },
+                {
+                    heading: ''
                 }
             ]
         }
