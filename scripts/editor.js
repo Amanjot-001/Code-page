71f0295d754677382814d1f3f30373acd0ff795b
Amanjot Singh <amanjotsingh2309@gmail.com>
2023-08-26 15:52:25 +0530

    styling changes

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,4 +1,4 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.setOption('enableLiveAutocompletion', true);
-editor.session.setMode("ace/mode/javascript");
+editor.session.setMode("ace/mode/html");
